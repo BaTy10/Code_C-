3c43c2794d8addb7d56e532da9185048df81e601
Ba Ty <tyba9188@tyba9188gmail.a9188gmailcom>
2024-10-13 10:12:44 +0700

    toan

--- demo.js
+++ demo.js
@@ -1,5 +1,16 @@
-console.log("xin chao cau ");
+
 let a = 10;
 let b = 20;
 let result = a + b;
+console.log(result);
+
+let firstName = "Ba";
+let lastName = "Ty";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+let x = 100;
+let y = 200;
+let z = 50;
+let result = x + y - z;
 console.log(result);
